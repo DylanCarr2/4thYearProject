cfedc96a36531ab05d768549bc45929d7ba26b7f
Dylan Carr <dylancarr2@gmail.com>
2023-02-06 18:27:02 +0000

    single page parse with buttons complete

--- scraper.py
+++ scraper.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("site.html")
-@app.route("/search", methods=['GET','POST'])
-def getAdvertsData():
+@app.route("/<query>/search<pageNum>", methods=['GET','POST'])
+
+def getAdvertsData(query,pageNum):
     output = request.form.to_dict()
-    query = output["searchbar"]
+    ##query = output["searchbar"]
     print(query)
-    baseURL = "https://www.adverts.ie/for-sale/q_"+query
+    baseURL = "https://www.adverts.ie/for-sale/q_"+query+"/page-"+pageNum
     ##initialising arrays of relevant data to scrape, for putting in JSON file for the site to read
     listingTitles=[]
     links = []
@@ -29,21 +30,17 @@ def getAdvertsData():
         pageNumbers.append(pageNumResult.text)
     print(pageNumbers)
     i = 0
-    while i < (int)(pageNumbers[-1]):
-        newURL = baseURL + "/page-" + str(i+1)
-        print(newURL)
-        advertsData = requests.get(newURL)
-        adSearchResults = adverts.find_all("div", class_="sr-grid-cell quick-peek-container")
+    adSearchResults = adverts.find_all("div", class_="sr-grid-cell quick-peek-container")
 
-        for adSearchResults in adSearchResults:
-            ##For images, need to extract just the image "src" tag, so needed this extra "get" method to parse the data
-            imgResult = adSearchResults.find("img", class_="main-ad-image")
-            listingTitles.append(adSearchResults.find("div", class_ ="title").text.replace("\n","").strip())
-            images.append(imgResult.get("src"))
-            prices.append(adSearchResults.find("div", class_ ="price").text.replace("\n","").strip())
-            locations.append(adSearchResults.find("div", class_ = "location").text.replace("\n","").strip())
-            links.append("https://www.adverts.ie" + adSearchResults.find("a", class_ ="main-image").get("href"))
-        i+=1
+    for adSearchResults in adSearchResults:
+        ##For images, need to extract just the image "src" tag, so needed this extra "get" method to parse the data
+        imgResult = adSearchResults.find("img", class_="main-ad-image")
+        listingTitles.append(adSearchResults.find("div", class_ ="title").text.replace("\n","").strip())
+        images.append(imgResult.get("src"))
+        prices.append(adSearchResults.find("div", class_ ="price").text.replace("\n","").strip())
+        locations.append(adSearchResults.find("div", class_ = "location").text.replace("\n","").strip())
+        links.append("https://www.adverts.ie" + adSearchResults.find("a", class_ ="main-image").get("href"))
+    i+=1
     print(listingTitles)
 
     file = open("static/js/scrapedInfo.json", "w")
--- script.js
+++
@@ -1,6 +0,0 @@
-getURL(searchPrefix)
-    {
-        let query =  "https://www.adverts.ie/for-sale/q_" + Document.getElementById("searchbar").innerHTML;
-
-        return query;
-    }

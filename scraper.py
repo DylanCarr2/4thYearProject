efd555a3cf0c030dd6a1270960ea9d84734c03f0
DylanCarr2 <113989270+DylanCarr2@users.noreply.github.com>
2023-04-28 00:16:10 +0100

    Emergency Bug Fixes
    Some last minute bug fixes to restore functionality to the project. Some minor bugs remain, but the big issues are resolved.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import re
+import os
 from bs4 import BeautifulSoup
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -12,9 +14,13 @@ locations=[]
 @app.route("/")
 def index():
     return render_template("site.html")
-@app.route("/<query>/search<pageNum>", methods=['GET','POST'])
-
+@app.route("/<query>/search/<pageNum>", methods=['GET','POST'])
 def getAdvertsData(query,pageNum):
+    listingTitles=[]
+    links = []
+    images=[]
+    prices=[]
+    locations=[]
     output = request.form.to_dict()
     print(query)
     baseURL = "https://www.adverts.ie/for-sale/q_"+query+"/page-"+pageNum
@@ -25,11 +31,7 @@ def getAdvertsData(query,pageNum):
     adverts = BeautifulSoup(advertsData.content, "html.parser")
     ## Finds all divs containing item information on the page
     adSearchResults = adverts.find("ul", class_ = "paging")
-    pageNumResult = adSearchResults.find_all("a")
-    pageNumbers = []
-    for pageNumResult in pageNumResult:
-        pageNumbers.append(pageNumResult.text)
-    print(pageNumbers)
+    
     adSearchResults = adverts.find_all("div", class_="sr-grid-cell quick-peek-container")
 
     for adSearchResults in adSearchResults:
@@ -37,16 +39,15 @@ def getAdvertsData(query,pageNum):
         imgResult = adSearchResults.find("img", class_="main-ad-image")
         listingTitles.append(adSearchResults.find("div", class_ ="title").text.replace("\n","").strip())
         images.append(imgResult.get("src"))
-        prices.append(float(adSearchResults.find("div", class_ ="price").text.replace("\n","").replace("€","").replace(",","").replace("FREE","0").strip()))
+        parsedPrice = adSearchResults.find("div", class_ ="price").get_text()
+        re.sub(r'[^0-9]', '', parsedPrice)
+        if(parsedPrice == ""):
+                parsedPrice = 0
+        prices.append(parsedPrice)
         locations.append(adSearchResults.find("div", class_ = "location").text.replace("\n","").strip())
         links.append("https://www.adverts.ie" + adSearchResults.find("a", class_ ="main-image").get("href"))
-    print(listingTitles)
 
-    getDoneDealData(query,pageNum)
-    return render_template("search.html")
-def getDoneDealData(query, pageNum):
-    output = request.form.to_dict()
-    print(query)
+    #below used to be getDoneDealData, combined functions together to fix a bug where the json file would multiply arrays.
     pageNum = int(pageNum)
     pageNum = (pageNum-1)*30
     baseURL = "https://www.donedeal.ie/all?words="+query+"&start="+str(pageNum)
@@ -56,21 +57,29 @@ def getDoneDealData(query, pageNum):
     advertsData = requests.get(baseURL)
     adverts = BeautifulSoup(advertsData.content, "html.parser")
     adverts = adverts.find("div", id = '__next').select_one("div[class *= 'DefaultLayout__Wrapper']").select_one("div[class *= 'styles__Container']").select_one("ul[class *= 'Listings__List']").select("li[class *= 'Listings__Desktop']")
-    print(adverts)
+    
 
     for adSearchResults in adverts:
-        ##For images, need to extract just the image "src" tag, so needed this extra "get" method to parse the data
-        listingTitles.append(adSearchResults.select_one("div[class *='Card__Body']").select_one("p[class *= 'Card__Title']").get_text())
-        images.append(adSearchResults.find("img").get("src"))
-        prices.append(float(adSearchResults.select_one("p[class *='Card__InfoText']").get_text().replace("€","").replace(",","").replace("FREE","0")))
-        keyInfo = adSearchResults.select("li[class *='Card__KeyInfoItem']")
-        locations.append(keyInfo[1].getText())
-        links.append(adSearchResults.select_one("a[class *= 'Link__SLinkButton']").get("href"))
-    print(listingTitles)
-    print(images)
-    print(prices)
-    print(locations)
-    print(links)
+        if not(adSearchResults.select_one("a[class *= 'Link__SLinkButton']") is None):
+            ##For images, need to extract just the image "src" tag, so needed this extra "get" method to parse the data
+            listingTitles.append(adSearchResults.select_one("div[class *='Card__Body']").select_one("p[class *= 'Card__Title']").get_text())
+            print(listingTitles)
+            if not(adSearchResults.find("img") == None):
+                images.append(adSearchResults.find("img").get("src"))
+            else:
+                images.append("No Image")
+            parsedPrice = adSearchResults.select_one("p[class *='Card__InfoText']").get_text()
+            re.sub(r'[^0-9]', '', parsedPrice)
+            #float(adSearchResults.select_one("p[class *='Card__InfoText']").get_text().replace("€","").replace(",","").replace("FREE","0")))
+            if(parsedPrice == ""):
+                parsedPrice = 0
+            prices.append(parsedPrice)
+            keyInfo = adSearchResults.select("li[class *='Card__KeyInfoItem']")
+            locations.append(keyInfo[1].getText())
+            
+            links.append(adSearchResults.select_one("a[class *= 'Link__SLinkButton']").get("href"))
+    if(os.path.isfile("static/js/scrapedInfo.json")):
+        os.remove("static/js/scrapedInfo.json") 
     file = open("static/js/scrapedInfo.json", "w")
     dumpedArray = json.dumps(listingTitles)
     file.write("[" + dumpedArray)
@@ -83,5 +92,6 @@ def getDoneDealData(query, pageNum):
     dumpedArray = json.dumps(links)
     file.write(","+dumpedArray+"]")
     file.close()
+    return render_template("search.html")
 if __name__ =='__main__':
     app.run(debug=True, port =5000)
--- static/js/searchResults.js
+++ static/js/searchResults.js
@@ -1,10 +1,13 @@
 //var searchInfo = $.getJSON("scrapedInfo.json");
 //var searchInfo = require('./scrapedInfo.json')
 //var searchInfo = JSON.parse("scrapedInfo.json");
+
+
 let jsonData;
 //currentPage = window.location.href.split().pop();
-currentPage = parseInt(window.location.href.substring(window.location.href.indexOf("/search") + 7));
-fetch('../static/js/scrapedInfo.json')
+currentPage = parseInt(window.location.href.substring(window.location.href.indexOf("/search") + 8));
+//fetch('../static/js/scrapedInfo.json')
+fetch('../../../static/js/scrapedInfo.json')
 .then((response) =>  response.json())
 .then(data => {
     jsonData = data;
@@ -26,14 +29,15 @@ fetch('../static/js/scrapedInfo.json')
         div = document.createElement("div");
         document.getElementById("searchResults").innerHTML += jsonData[0][i] 
         +"<br> <img src=' "+jsonData[1][i]+"' >"
-        +"<br> <b>"+"€"+jsonData[2][i]+"</b> <br>"
+        +"<br> <b>"+jsonData[2][i]+"</b> <br>"
         +jsonData[3][i]
         +"<br>"+"<a href='"+jsonData[4][i]+"'>"+jsonData[4][i]+"</a><br><br>";
     }
     currentURL = window.location.href.slice(0,window.location.href.lastIndexOf("/search"));
     document.getElementById("pageButtons").innerHTML = 
-    '<a href='+currentURL+'/search'+(currentPage-1)+' class="previous">&laquo; Previous</a>'+
-    '<a href='+currentURL+'/search'+(currentPage+1)+' class="next">Next &raquo;</a>'
+    '<a href='+currentURL+'/search/'+(currentPage-1)+' class="previous">&laquo; Previous</a>'+
+    '<a href='+currentURL+'/search/'+(currentPage+1)+' class="next">Next &raquo;</a>';
+    
 })
 
 //searchDocument.getElementById("searchResults").innerHTML;
